c9fdc65433ab44e74b09b5a2467fa73eeaae8ff0
Grant Robinson <Chisdeal2014@hotmail.com>
2021-02-24 12:16:05 +0000

    Update index.js
    added gamesdata and gamesdatamaps allow describe Game and they Maps of that games
    
    i added cryptoinfo for part tell you official Crypto info.

--- src/index.js
+++ src/index.js
@@ -25,3 +25,26 @@ module.exports.vrchatmaps = function(query) {
 
     return file;
 }
+
+module.exports.gamesdatamaps = function(game, query) {
+    const file = require(`./games/${game}/maps/${query}`);
+    if(!file) return console.log(`404`);
+
+    return file;
+}
+
+
+module.exports.gamesdata = function(game, query) {
+    const file = require(`./games/${game}/${query}`);
+    if(!file) return console.log(`404`);
+
+    return file;
+}
+
+
+module.exports.cryptoinfo = function(query) {
+    const file = require(`./cryptocurrency/${query}`);
+    if(!file) return console.log(`404`);
+
+    return file;
+}
