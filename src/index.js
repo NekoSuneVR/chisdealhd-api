e38aa6c0fd31bcb5b84ac5a0e9730a7c6e02abc0
Grant Robinson <Chisdeal2014@hotmail.com>
2021-02-24 12:21:11 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,12 +19,12 @@ module.exports.vrchatfans = function(query) {
     return file;
 }
 
-module.exports.vrchatmaps = function(query) {
+/*module.exports.vrchatmaps = function(query) {
     const file = require(`./vrchat/maps/${query}`);
     if(!file) return console.log(`404`);
 
     return file;
-}
+}*/
 
 module.exports.gamesdatamaps = function(game, query) {
     const file = require(`./games/${game}/maps/${query}`);
